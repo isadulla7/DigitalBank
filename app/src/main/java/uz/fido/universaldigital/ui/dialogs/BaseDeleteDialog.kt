aba57286d1b11213a2d17d79150027170dff9272
Jake <madaminov0998@gmail.com>
2023-12-28 11:45:13 +0500

    git

---
+++ app/src/main/java/uz/fido/universaldigital/ui/dialogs/BaseDeleteDialog.kt
@@ -0,0 +1,35 @@
+import android.os.Bundle
+import uz.fido.universaldigital.base.BaseDialogFragment
+import uz.fido.universaldigital.databinding.BaseDialogDeleteBinding
+
+class BaseDeleteDialog(
+    private var title: String,
+    private var subtitle: String,
+    private var setOnClickDelete: () -> Unit
+) : BaseDialogFragment<BaseDialogDeleteBinding>(BaseDialogDeleteBinding::inflate) {
+
+    override fun onInit(savedInstanceState: Bundle?) {
+        super.onInit(savedInstanceState)
+
+        initDetails()
+        initSetOnClickListeners()
+    }
+
+    private fun initDetails() {
+        binding.apply {
+            title.text = this@BaseDeleteDialog.title
+            subtitle.text = this@BaseDeleteDialog.subtitle
+        }
+    }
+
+    private fun initSetOnClickListeners() {
+        binding.dismiss.setOnClickListener {
+            dismiss()
+        }
+        binding.ok.setOnClickListener {
+            setOnClickDelete.invoke()
+            dismiss()
+        }
+    }
+
+}
