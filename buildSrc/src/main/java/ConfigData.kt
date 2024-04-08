06269b4755f8cfd6877ff2d163c837f1ee1b0244
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-04-08 16:55:09 +0500

    Revert "bug fixed"
    This reverts commit aa3e30b6fe432b7b08cc27bb19fd47ab7cce679b.

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -4,10 +4,10 @@ object ConfigData {
     const val minSdkVersion = 23
     const val compileSdkVersion = 34
     const val targetSdkVersion = 34
-    const val buildToolsVersion = "33.0.0"
+    const val buildToolsVersion = "34.0.0"
 
     const val versionCode = 25
-    const val releaseVersionName = "1.0.1"
+    const val releaseVersionName = "1.0.2"
     const val archivesBaseName = "Universalbank($versionCode)"
 
 }
