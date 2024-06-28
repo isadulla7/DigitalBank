f5c86e834e7251d9713d791bfaf870d5ebe3fdb0
Jake <madaminov0998@gmail.com>
2024-06-28 15:26:52 +0500

    Select branch option added to deposit creation

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -1,12 +1,12 @@
 object ConfigData {
 
-    const val APPLICATION_ID = "uz.fido.universaldigital"
+    const val APPLICATION_ID = "uz.fido.local.universaldigital"
     const val MIN_SDK_VERSION = 23
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION: String = "34.0.0"
-    const val VERSION_CODE = 63
-    const val RELEASE_VERSION_NAME = "1.0.15"
+    const val VERSION_CODE = 1
+    const val RELEASE_VERSION_NAME = "1.0.0"
     const val ARCHIVES_BASE_NAME = "Universalbank($VERSION_CODE)"
 
 }
