17da8e29d1cdf653b2efe99f06d46e2d937b9b69
Jake <madaminov0998@gmail.com>
2024-07-01 11:23:26 +0500

    Select branch option added to deposit creation

--- network/src/main/cpp/network-lib.cpp
+++ network/src/main/cpp/network-lib.cpp
@@ -13,14 +13,12 @@ Java_uz_fido_network_di_Keys_getUserInfoUrl(JNIEnv *env, jobject thiz) {
 JNIEXPORT jstring
 Java_uz_fido_network_di_Keys_getClientId(JNIEnv *env, jobject thiz) {
     char *clientId = "-3";
-//    char *clientId = "-2";
     return env->NewStringUTF(clientId);
 }
 
 JNIEXPORT jstring
 Java_uz_fido_network_di_Keys_getBaseUrl(JNIEnv *env, jobject thiz) {
     char *baseUrl = "https://ra.ubank.uz/api/";
-//    char *baseUrl = "http://my.fido.uz:9192/api/";
     return env->NewStringUTF(baseUrl);
 }
 
