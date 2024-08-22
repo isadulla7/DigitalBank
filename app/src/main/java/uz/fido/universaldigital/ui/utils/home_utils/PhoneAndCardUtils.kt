70a9910849d66a1c4cafd65f6aea3167fdc237dd
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-08-22 13:58:34 +0500

    add widget

---
+++ app/src/main/java/uz/fido/universaldigital/ui/utils/home_utils/PhoneAndCardUtils.kt
@@ -0,0 +1,43 @@
+package uz.fido.universaldigital.ui.utils.home_utils
+
+import android.widget.Toast
+import io.paperdb.Paper
+import uz.fido.universaldigital.R
+import uz.fido.utils.const.Const
+
+fun applyMask(mask: String, text: String): String {
+    var maskedText = ""
+    var index = 0
+
+    for (char in mask) {
+        if (char == '#') {
+            if (index < text.length) {
+                maskedText += text[index]
+                index++
+            } else {
+                break
+            }
+        } else {
+            maskedText += char
+        }
+    }
+
+    return maskedText
+}
+
+fun mobileServiceId(text:String):String{
+   return when (text.substring(3, 5)) {
+        "90", "91" -> {
+            "51"
+        }
+
+        "93", "94" -> {
+            "53"
+        }
+
+        "99", "95" -> "687"
+        "97", "88" -> "54"
+        "98" -> "52"
+        else -> "error"
+    }
+}
