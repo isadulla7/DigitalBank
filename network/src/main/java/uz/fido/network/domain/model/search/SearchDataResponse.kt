3eb2aea262b949e45a4b41b44dacf46ec4e3f8a2
Jake <madaminov0998@gmail.com>
2024-08-28 11:20:40 +0500

    Auto identify dialog added

--- network/src/main/java/uz/fido/network/domain/model/search/SearchDataResponse.kt
+++ network/src/main/java/uz/fido/network/domain/model/search/SearchDataResponse.kt
@@ -31,6 +31,7 @@ class SearchDataResponse(
     var to_object_currency: String? = null,
     var to_object_expire: String? = null,
     var to_embossed_name: String? = null,
+    var from_embossed_name: String? = null,
     var get_info: String? = null,
     var token: String? = null,
     var to_object_id: String? = null,
