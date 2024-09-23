92198071231c66583a75de9c6837330ed3686e9b
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-09-23 15:45:24 +0500

    bug fixed

---
+++ app/src/main/java/uz/fido/universaldigital/ui/fragments/products/widgets/search/model/SearchItemMapper.kt
@@ -0,0 +1,12 @@
+package uz.fido.universaldigital.ui.fragments.products.widgets.search.model
+
+import uz.fido.network.domain.model.search.LocalSearchDto
+import uz.fido.network.room.LocalSearchDao
+
+/*
+fun SearchItem.action() = LocalSearchDto(
+    serviceGroupCode = serviceGroupCode,
+    groupCode = groupCode,
+
+
+)*/
