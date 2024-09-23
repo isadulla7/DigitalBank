aaa1541faa617dca9243411b63b2e604164ecfcc
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-09-23 15:38:25 +0500

    bug fixed

--- app/src/main/java/uz/fido/universaldigital/ui/fragments/payment/download_payment/DownloadPayment.kt
+++ app/src/main/java/uz/fido/universaldigital/ui/fragments/payment/download_payment/DownloadPayment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import io.paperdb.Book
+import io.paperdb.Paper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -17,8 +19,6 @@ import uz.fido.universaldigital.ui.fragments.products.widgets.search.SearchList
 import uz.fido.universaldigital.ui.fragments.products.widgets.search.SearchList.GROUP_NAME_PAYMENT
 import uz.fido.universaldigital.ui.fragments.products.widgets.search.SearchList.GROUP_NAME_PAYMENT_GROUP
 import uz.fido.universaldigital.ui.fragments.products.widgets.search.model.SearchItem
-import uz.fido.universaldigital.ui.utils.extensions.getFromPaper
-import uz.fido.universaldigital.ui.utils.extensions.saveToPaper
 import uz.fido.utils.const.Const
 
 @AndroidEntryPoint
@@ -29,6 +29,7 @@ abstract class DownloadPayment : Fragment() {
 
     var databaseHelper: DatabaseHelper? = null
     var paymentGroupsList: ArrayList<PaymentGroup> = ArrayList()
+     var updateLang:Boolean=false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,22 +38,14 @@ abstract class DownloadPayment : Fragment() {
         databaseHelper = DatabaseHelper(requireContext())
     }
 
-    fun downloadCheckLang() {
-        databaseHelper = DatabaseHelper(requireContext())
-        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default) {
-            databaseHelper?.let { databaseHelper ->
-                if (databaseHelper.getGroupList().size != 0) {
-                    Log.d("TAG", "downloadCheckLang:${databaseHelper.getGroupList()[0].name} ")
-                    paymentGroupsList = databaseHelper.getGroupList()
-                    paymentGroupsList.sortBy { it.order }
-                    withContext(Dispatchers.Main) {
-                        downloadPaymentInterface.fetchCompleteFromDB()
-                    }
-                }
-            }
+
+
+  fun checkLang() {
+        try {
+            updateLang= Paper.book().read(Const.UPDATE_LANG)
+        }catch (e:Exception){
+            updateLang=false
         }
-
-
     }
 
 
@@ -61,12 +54,14 @@ abstract class DownloadPayment : Fragment() {
     }
 
     fun checkForPaymentDownload() {
-        val currentDatabaseVersion = getFromPaper(Const.PAPER_PAYMENT_VERSION, "0")
-        val savedDatabaseVersion = getFromPaper(Const.PAPER_PAYMENT_VERSION_DB, "0")
-        if (downloadPaymentViewModel.paymentGroupMutableList.value != null && downloadPaymentViewModel.paymentGroupMutableList.value!!.size != 0
+
+        val currentDatabaseVersion = Paper.book().read(Const.PAPER_PAYMENT_VERSION, "0")
+        val savedDatabaseVersion = Paper.book().read(Const.PAPER_PAYMENT_VERSION_DB, "0")
+        if (downloadPaymentViewModel.paymentGroupMutableList.value != null && downloadPaymentViewModel.paymentGroupMutableList.value!!.size != 0 && !updateLang
         ) {
             downloadPaymentInterface.getMutablePaymentList()
         } else {
+            Paper.book().write(Const.UPDATE_LANG,false)
             if (currentDatabaseVersion == savedDatabaseVersion) {
                 getPaymentsFromLocal()
             } else {
@@ -75,7 +70,7 @@ abstract class DownloadPayment : Fragment() {
         }
     }
 
-    private fun getPaymentsFromLocal() {
+     private fun getPaymentsFromLocal() {
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Default) {
             databaseHelper?.let { databaseHelper ->
                 if (databaseHelper.getGroupList().size != 0) {
@@ -119,13 +114,15 @@ abstract class DownloadPayment : Fragment() {
                 databaseHelper.insertCashbackList(payment.cashback_list ?: ArrayList())
                 databaseHelper.insertReferenceList(payment.references_list ?: ArrayList())
                 paymentGroupsList = databaseHelper.getGroupList()
+                withContext(Dispatchers.Main){
                 downloadPaymentInterface.downloadPaymentSuccess()
-                saveToPaper(Const.PAPER_PAYMENT_VERSION_DB, payment.curr_version ?: "0")
+                }
+                Paper.book().write(Const.PAPER_PAYMENT_VERSION_DB, payment.curr_version ?: "0")
             }
         }
     }
 
-    fun fillSearchList() {
+  /*  fun fillSearchList() {
         val paymentService = ArrayList<SearchItem>()
         val paymentGroups = ArrayList<SearchItem>()
         paymentGroupsList.forEach { paymentGroup ->
@@ -155,6 +152,6 @@ abstract class DownloadPayment : Fragment() {
         SearchList.addList(paymentService)
         SearchList.addList(paymentGroups)
         SearchList.saveSearchList(requireActivity())
-    }
+    }*/
 
 }
--- network/src/main/java/uz/fido/network/room/LocalPaymentDatabase.kt
+++ network/src/main/java/uz/fido/network/room/LocalPaymentDatabase.kt
@@ -4,9 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import uz.fido.network.domain.model.payment.location.LocalPayment
 import uz.fido.network.domain.model.payment.location.LocalPaymentType
+import uz.fido.network.domain.model.search.LocalSearchDto
 
-@Database(entities = [LocalPayment::class,LocalPaymentType::class], version = 1, exportSchema = false)
+@Database(entities = [LocalPayment::class,LocalPaymentType::class,LocalSearchDto::class], version = 2, exportSchema = false)
 abstract class LocalPaymentDatabase:RoomDatabase() {
 abstract fun localPaymentDao():LocalPaymentDao
 abstract fun localPaymentTypeDao():LocalPaymentTypeDao
+abstract fun localSearchDao():LocalSearchDao
 }
