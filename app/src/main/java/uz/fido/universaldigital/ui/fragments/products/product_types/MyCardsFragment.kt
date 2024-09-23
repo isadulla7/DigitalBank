05376f30a32afad12b9e94bdc545a0ef00483853
Jake <madaminov0998@gmail.com>
2024-09-23 16:48:42 +0500

    Paper lib saving and getting logic changed

--- app/src/main/java/uz/fido/universaldigital/ui/fragments/products/product_types/MyCardsFragment.kt
+++ app/src/main/java/uz/fido/universaldigital/ui/fragments/products/product_types/MyCardsFragment.kt
@@ -30,11 +30,9 @@ import uz.fido.universaldigital.ui.fragments.products.cards.dialogs.CloseWalletD
 import uz.fido.universaldigital.ui.fragments.products.cards.dialogs.DeleteCardDialog
 import uz.fido.universaldigital.ui.fragments.products.cards.dialogs.ShareCardNumberDialog
 import uz.fido.universaldigital.ui.fragments.products.cards.dialogs.WalletOperationsDialog
-import uz.fido.universaldigital.ui.fragments.services.order_card.CardType
 import uz.fido.universaldigital.ui.utils.choose_card.BaseCardUtils.getLayoutManager
 import uz.fido.universaldigital.ui.utils.choose_card.BaseCardUtils.getSpanCount
 import uz.fido.utils.const.CardConst
-import uz.fido.utils.const.CardConst.UZCARD
 import uz.fido.utils.const.Const
 import uz.fido.utils.utility.fragment.goto
 import uz.fido.utils.utility.user.getClientToken
@@ -181,8 +179,8 @@ class MyCardsFragment : BaseSimpleFragment<FragmentAllCardsBinding>(
             R.id.monitoring -> {
                 cardOperationsDialog.dismiss()
                 Log.d("TAG", "onClick:${selectedCard.object_id} ")
-                when (selectedCard.object_type){
-                    CardConst.UZCARD ->goto(R.id.uzCardMonitoringFragment, bundleOf(Const.CARD to selectedCard))
+                when (selectedCard.object_type) {
+                    CardConst.UZCARD -> goto(R.id.uzCardMonitoringFragment, bundleOf(Const.CARD to selectedCard))
                     CardConst.HUMO_CARD -> goto(R.id.humoMonitoringFragment, bundleOf(Const.CARD to selectedCard))
                     CardConst.WALLET -> goto(R.id.walletMonitoringFragment, bundleOf(Const.CARD to selectedCard))
                     //else->goto(R.id.visaMonitoringFragment, bundleOf(Const.CARD to selectedCard))
@@ -237,6 +235,7 @@ class MyCardsFragment : BaseSimpleFragment<FragmentAllCardsBinding>(
                         hideProgress()
                         it.data?.let { cardListResponse ->
                             cardListResponse.objects?.let { it1 ->
+                                refreshCards()
                                 cardsAdapter?.submitList(it1)
                                 cardsAdapter?.notifyDataSetChanged()
                                 menuProductsViewModel.updateCards(it1)
