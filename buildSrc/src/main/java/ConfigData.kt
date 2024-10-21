e5aaecd504e68a4c53da8be05a659a3ddb2b26c2
Jake <madaminov0998@gmail.com>
2024-10-21 15:47:03 +0500

    Transfer max amount changes added

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -5,7 +5,7 @@ object ConfigData {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION: String = "34.0.0"
-    const val VERSION_CODE = 88
+    const val VERSION_CODE = 89
     const val RELEASE_VERSION_NAME = "1.0.26"
     const val ARCHIVES_BASE_NAME = "Universalbank($VERSION_CODE)"
 
