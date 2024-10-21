0e098d7a34e5bb23d6fc379358e2bdc5cdac5b1b
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-10-21 15:45:34 +0500

    local monitoring bug fixed

--- app/src/main/java/uz/fido/universaldigital/ui/fragments/monitoring/filter/MonitoringFilterFragment.kt
+++ app/src/main/java/uz/fido/universaldigital/ui/fragments/monitoring/filter/MonitoringFilterFragment.kt
@@ -328,31 +328,7 @@ class MonitoringFilterFragment : BaseFragment<FragmentMonitoringFilterBinding, M
                         }
                     }
                     if (cardList.isEmpty()) {
-                        cardList.add(
-                            FilterCard(
-                                0,
-                                getString(R.string.activ),
-                                "",
-                                "",
-                                "",
-                                false,
-                                1
-                            )
-                        )
                         cardList.addAll(activeList)
-                        cardList.add(
-                            FilterCard(
-                                0,
-                                getString(R.string.no_activ),
-                                "",
-                                "",
-                                "",
-                                false,
-                                1
-                            )
-                        )
-                        cardList.addAll(passiveList)
-
                     }
                     successCardList(cardList)
                 }
