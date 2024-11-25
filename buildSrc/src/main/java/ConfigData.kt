b5f5941bd68609adf56c589e9e42517d48766e23
jamshid.eshboboyev <jamshid.eshboboyev@fido.uz>
2024-11-25 09:50:33 +0500

    bug list and security

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -5,8 +5,8 @@ object ConfigData {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION: String = "34.0.0"
-    const val VERSION_CODE = 94
-    const val RELEASE_VERSION_NAME = "1.0.27"
+    const val VERSION_CODE = 95
+    const val RELEASE_VERSION_NAME = "1.0.29"
     const val ARCHIVES_BASE_NAME = "Universalbank($VERSION_CODE)"
 
 }
