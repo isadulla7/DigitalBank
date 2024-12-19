095c5743b58f7780631cd7637d3fa9f0e281a9b7
Jake <madaminov0998@gmail.com>
2024-12-19 11:24:25 +0500

    Fix: Transfer by phone/wallet issues
    - Fixed transfer by phone/wallet related issues
    - Restricted SMS confirmation field to only accept numbers
    - Updated version code to 100 and version name to 1.0.29
    - Fixed potential crash in TransferToCardUseCase
    - Prevented crash when selecting a photo from gallery
    - Updated dialog theme
    - Fixed potential crash in MyHouseSinglePaymentFragment
    - Changed text color for the 'What is UZCARD-MIR?' link
    - Updated styling for transfer by phone and transfer by wallet amount text
    - Removed unused APK and AAB files

--- utils/src/main/java/uz/fido/utils/utility/user/PaperExt.kt
+++ utils/src/main/java/uz/fido/utils/utility/user/PaperExt.kt
@@ -39,14 +39,14 @@ fun Fragment.getFromPaper(key: String, defaultValue: String? = ""): String {
 
 fun Context.getFromPaper(key: String, defaultValue: String? = ""): String {
     val encryptedValue = Paper.book().read<String>(key, defaultValue)
-    val decryptedValue: String
+    var decryptedValue: String = ""
     try {
         decryptedValue = CryptoUtil.decrypt(encryptedValue, getDeviceIds())
         Log.d("TAG", "getFromPaper:$decryptedValue")
 
     } catch (e: Exception) {
-        return encryptedValue.orEmpty()
         Log.d("TAG", "getFromPaper:----$decryptedValue")
+        return encryptedValue.orEmpty()
 
     }
     return decryptedValue
