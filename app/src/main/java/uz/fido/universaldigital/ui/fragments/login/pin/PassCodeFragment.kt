473cf49cef7250b0c72a7c8109c0df8e25f0d0a7
Jake <madaminov0998@gmail.com>
2025-01-27 11:18:58 +0500

    latest commit

--- app/src/main/java/uz/fido/universaldigital/ui/fragments/login/pin/PassCodeFragment.kt
+++ app/src/main/java/uz/fido/universaldigital/ui/fragments/login/pin/PassCodeFragment.kt
@@ -81,7 +81,11 @@ class PassCodeFragment : BaseFragment<FragmentPassCodeBinding, PinCodeViewModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         try {
-            arguments?.let { operation = it.getString(Const.OPERATION, "") }
+            if (arguments != null) {
+                if (arguments?.getString(Const.OPERATION, "") != null) {
+                    operation = requireArguments().getString(Const.OPERATION, "")
+                }
+            }
         } catch (e: Exception) {
             recordException(e, ::onCreate.name)
         }
@@ -204,19 +208,17 @@ class PassCodeFragment : BaseFragment<FragmentPassCodeBinding, PinCodeViewModel>
 
     private fun fingerAuth() {
         val executor = Executors.newSingleThreadExecutor()
-        val biometricPrompt =
-            BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
-
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    requireActivity().runOnUiThread {
-                        fillDots()
-                        pin = getDecodedString(getFromPaper(Const.PAPER_CLIENT_PIN))
-                        swapKeys()
-                    }
+        val biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                requireActivity().runOnUiThread {
+                    fillDots()
+                    pin = getDecodedString(getFromPaper(Const.PAPER_CLIENT_PIN))
+                    swapKeys()
                 }
+            }
 
-            })
+        })
         val promptInfo = BiometricPrompt.PromptInfo.Builder()
             .setTitle(getString(R.string.enter_app_with_touch_id))
             .setNegativeButtonText(getString(R.string.cancel)).build()
