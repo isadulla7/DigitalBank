30745ea304e9b19bf50d978e6c8d352e9b41aaa2
Jake <madaminov0998@gmail.com>
2025-03-14 09:58:07 +0500

    Code optimization, firebase crashlytics errors fixed

--- buildSrc/src/main/java/PluginDependencies.kt
+++ buildSrc/src/main/java/PluginDependencies.kt
@@ -9,4 +9,5 @@ object PluginDependencies {
     const val uzInfoComMyId = "https://storage.uzinfocom.uz/libs/android/myid"
     const val flutterIo = "https://storage.googleapis.com/download.flutter.io"
     const val androidToolsR8 = "com.android.tools:r8:8.5.35"
+    const val firebasePerf = "com.google.firebase:perf-plugin:1.4.2"
 }
