6e3bad4fcdd89ba4bde57703fa2e89304df175ae
Jake <madaminov0998@gmail.com>
2025-05-01 17:24:45 +0500

    Til o'zgarganda headerda ham o'zgaradigan qilindi

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -4,7 +4,7 @@ object ConfigData {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION: String = "34.0.0"
-    const val VERSION_CODE = 154
+    const val VERSION_CODE = 155
     const val RELEASE_VERSION_NAME = "1.0.40"
     const val ARCHIVES_BASE_NAME = "Universalbank-$RELEASE_VERSION_NAME($VERSION_CODE)"
 }
