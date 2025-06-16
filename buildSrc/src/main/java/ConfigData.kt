ea2e185290cbee513a9b218ce7da05b3e9e81af8
Jake <husniddinfido@gmail.com>
2025-06-16 14:58:52 +0500

    Edit profile fragmentda rasm yuklash paytida save button disable qilib qo'yildi

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -4,7 +4,7 @@ object ConfigData {
     const val COMPILE_SDK_VERSION = 34
     const val TARGET_SDK_VERSION = 34
     const val BUILD_TOOLS_VERSION: String = "34.0.0"
-    const val VERSION_CODE = 183
-    const val RELEASE_VERSION_NAME = "1.0.43"
+    const val VERSION_CODE = 184
+    const val RELEASE_VERSION_NAME = "1.0.44"
     const val ARCHIVES_BASE_NAME = "Universalbank-$RELEASE_VERSION_NAME($VERSION_CODE)"
 }
